f8a03125111432707d28b876f01111d586aef88b
PLD908 <oluyededivine@gmail.com>
2023-12-14 20:50:43 +0100

    Added some code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var x = document.getElementById('sign-up')
   x.style.display = "none";
 
+// var x = document.getElementById('menu')
+//   x.style.display = "none";
+
 function signUp() {
    var x = document.getElementById('sign-up')
      x.style.display = "block";
@@ -16,3 +19,13 @@ function signIn() {
      var y = document.getElementById('sign-up')
      y.style.display = "none";
 }
+
+// function dropDown() {
+//   var x = document.getElementById('ourNav').innerHTML;
+
+//     if (x.style.display === "block") {
+//         x.style.display = "block";
+//     }else {
+//         x.style.display = "none";
+//     }
+// }
